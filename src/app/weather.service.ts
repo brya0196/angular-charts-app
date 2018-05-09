02149ef1caa4e10bed7c62de9d7e463d05e95855
Bryan Astacio <brya0196@gmail.com>
2018-05-09 10:32:22 -0400

    added the function to the service and cosumed by app.component.ts to display the chart.js

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class WeatherService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  dailyForecast() {
+    return this._http.get("http://samples.openweathermap.org/data/2.5/history/city?q=London,UK&appid=b1b15e88fa797225412429c1c50c122a1")
+      .map(result => result);
+  }
+
+  
 
 }
